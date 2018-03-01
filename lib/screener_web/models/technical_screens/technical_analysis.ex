e538d5655312a0b4f6a31706796605b5e7c6462c
mtanca <mark.tanca@ipsoft.com>
2018-03-01 14:29:27 -0500

    parallelized work through processes (Task.async)

--- lib/screener_web/models/technical_screens/technical_analysis.ex
+++ lib/screener_web/models/technical_screens/technical_analysis.ex
@@ -1,9 +1,11 @@
 defmodule ScreenerWeb.TechnicalAnalysis do
   alias ScreenerWeb.BollingerBands
+  alias Screener.RelativeStrengthIndex
 
   def get_indicators(quotes) do
-    bb = BollingerBands.bollinger_bands(quotes)
-    %{"Bollinger Bands" => bb}
+    bb = Task.async fn -> BollingerBands.bollinger_bands(quotes) end
+    rsi = Task.async fn -> RelativeStrengthIndex.rsi(quotes) end
+    %{"Bollinger Bands" => Task.await(bb), "RSI" => Task.await(rsi)}
   end
 
 end
